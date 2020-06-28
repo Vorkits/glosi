9c4f30647d725557754bd5d6ec94940e9163b13b
Vorkits <reazer381@gmail.com>
2020-06-28 13:34:53 +0600

    ss

--- client/handlers.py
+++ client/handlers.py
@@ -147,7 +147,7 @@ def start_bot(config):
             sq=sql_query('SELECT tid from workers WHERE category={}'.format(to_base(zakazi[message.from_user.username]['category'])))
             markup=types.InlineKeyboardMarkup()
             print(sq)
-            markup.add(types.InlineKeyboardButton(text='Просмотреть контакты',callback_data='show{},{},{},{}'.format(city,name,phone,message.from_user.id)))
+            markup.add(types.InlineKeyboardButton(text='Просмотреть контакты',callback_data='show'))
             print('show{},{},{},{}'.format(city,name,phone,message.from_user.id))
             mes="""
             Новая заявка
