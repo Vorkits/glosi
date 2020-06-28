b8d973bc1c5ddafbc73f54f2dfec37e28c53a97e
Vorkits <reazer381@gmail.com>
2020-06-28 16:48:11 +0600

    ss

--- worker/handlers.py
+++ worker/handlers.py
@@ -6,24 +6,23 @@ users={}
 zakazi={}
 to_base=lambda s:"'"+str(s)+"'"
 def sql_query(sql):
+    f=''
     try:
         conn = psycopg2.connect(dbname='dbl897lkr3kqj0', user='gyvyzqvrydwlxe', 
                                 password='44826dd7802986c134535b8c9724edbe06185e69c372d144a0f554ad59bdf040', host='ec2-79-125-26-232.eu-west-1.compute.amazonaws.com')
         conn.autocommit = True
-    except:
-        print ("Cannot connect to db")
 
-    cur = conn.cursor()
-
-    try:
+        cur = conn.cursor()
         cur.execute(sql)
-    except:
-        print ("Cannot insert")
+        f=cur.fetchall()
+        conn.close()
+    except Exception as e:
+        print (e)
     try:
         conn.close()
     except:
-        print ("Cannot insert")
-    conn.close()
+        print()
+    return f
 
 def reply_city():
     markup=types.ReplyKeyboardMarkup(one_time_keyboard = True)
